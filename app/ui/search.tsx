8185cf051dc0b5c67f083f6bbe29c528e5c0f604
conradoPino <conradopino2@gmail.com>
2025-04-15 20:16:49 -0300

    .

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -8,9 +8,10 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term) => {
     console.log(`Searching... ${term}`);
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
@@ -18,7 +19,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     }
     replace(`${pathname}?${params.toString()}`);
     console.log(term);
-  }
+  }, 300);
  
   return (
     <div className="relative flex flex-1 flex-shrink-0">
